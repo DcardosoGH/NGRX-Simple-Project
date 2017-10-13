724a27665726714d0cb4c2c618dfc2adc6d894c3
Diogo Cardoso <bydiogo.cardoso@bycom.pt>
2017-10-13 16:04:54 +0100

    - NGRX

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Store  } from '@ngrx/store';
+import {Observable} from 'rxjs/Observable';
+import * as PostActions from './actions/post.actions';
+import { Post } from './models/post.model';
+
+interface AppState {
+  post: Post;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  post: Observable<Post>;
+  text: string;
+
+  constructor(private store: Store<AppState>) {
+    this.post = this.store.select('post');
+  }
+
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text));
+  }
+
+  upVote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+
+  downVote() {
+    this.store.dispatch(new PostActions.Downvote());
+  }
+
+  Reset() {
+    this.store.dispatch(new PostActions.Reset());
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule,  } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { simpleReducer } from './simple.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { postReducer } from './reducers/post.reducer';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    StoreModule.forRoot({
+      post: postReducer
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
